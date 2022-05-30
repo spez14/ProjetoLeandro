6e12ebd0eb2705925089c6a534032c65cb5cc056
Vitor Henrique dos Santos Pezzuol <vpezzuol@gmail.com>
2022-05-30 14:45:38 -0300

    Add files via upload

---
+++ ProjetoLeandro/SistemaCarros(ver04)/loginUsuario.php
@@ -0,0 +1,31 @@
+<?php
+
+    require_once "database.php";
+
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM ads_vitor_motors_users WHERE name = '$name' AND password = '$password'";
+
+    if($name==['name'], $password == ['password']) 
+    {
+        if(isset($submit))
+    {
+        $found = mysqli_query($conexao, $sql);
+
+        if(mysqli_num_rows($found) <= 0)
+        {
+            echo "Usuário e/ou senha incorretos.";
+        } else
+        {
+            setcookie("name", $name);
+            header("Location: painelUsuario.php");
+        }
+    }
+    } else
+    {
+        echo "Usuário e/ou senha incorretos.";
+        exit();
+    }
+?>
+
